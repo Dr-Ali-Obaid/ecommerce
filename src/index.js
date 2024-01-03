098a9c4d90663b1ce83ebd1cf4dafdc620679a6c
Dr-Ali-Obaid <dr.ali.emam@gmail.com>
2024-01-04 01:26:31 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -6,9 +6,27 @@ import '@fortawesome/fontawesome-free/js/all.min.js'
 
 
 document.querySelectorAll('[data-bs-toggle ="tooltip"]').forEach(item => new bootstrap.Tooltip(item));
-document.querySelectorAll(".add-to-cart-btn").forEach(i =>{
-    i.addEventListener('click', ()=>{
+document.querySelectorAll(".add-to-cart-btn").forEach(i => {
+    i.addEventListener('click', () => {
         alert("تم إضافة المنتج إلى السلة")
+    })
+});
+
+document.querySelectorAll('.size-option [type ="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active')
+        });
+        item.parentNode.parentNode.classList.add('active')
+    })
+});
+
+document.querySelectorAll('.color-option [type ="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active')
+        });
+        item.parentNode.parentNode.classList.add('active')
     })
 });
 
