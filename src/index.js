d3c32f8c16fc9a90ae283c8e8bb4d7dbe74c5478
Dr-Ali-Obaid <dr.ali.emam@gmail.com>
2024-01-06 23:36:21 +0300

    checkout page done

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ document.querySelectorAll('.size-option [type ="radio"]').forEach(item => {
             i.classList.remove('active')
         });
         item.parentNode.parentNode.classList.add('active')
-    })
+    });
 });
 
 document.querySelectorAll('.color-option [type ="radio"]').forEach(item => {
@@ -27,7 +27,35 @@ document.querySelectorAll('.color-option [type ="radio"]').forEach(item => {
             i.classList.remove('active')
         });
         item.parentNode.parentNode.classList.add('active')
-    })
+    });
 });
+
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+    item.addEventListener('change', () => {
+        const newQuantity = item.value;
+        const parent = item.closest('[data-product-info]');
+        const pricePerProduct = parent.getAttribute('data-product-price');
+        const totalPrice = newQuantity * pricePerProduct;
+        parent.querySelector('.total-price-for-product').innerHTML = totalPrice + '$';
+        totalAdjustedPrice();
+    });
+});
+document.querySelectorAll('[data-remove-from-cart]').forEach(i =>{
+    i.addEventListener('click', ()=>{
+        i.closest('[data-product-info]').remove();
+        totalAdjustedPrice();
+    });
+});
+
+function totalAdjustedPrice(){
+    let totalOfAllProducts = 0;
+        document.querySelectorAll('[data-product-info]').forEach(product => {
+            const pricePerUnit = product.getAttribute('data-product-price');
+            const quantity = product.querySelector('[data-product-quantity]').value;
+            const totalPerUnit = quantity * pricePerUnit;
+            totalOfAllProducts += totalPerUnit;
+        });
+        document.getElementById('total-price-for-all-products').innerHTML = totalOfAllProducts;
+};
 
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة سنة " + new Date().getFullYear() + "م";
