5d90fa3461c8bb05d095f68455a836df15accf01
Dr-Ali-Obaid <dr.ali.emam@gmail.com>
2024-01-17 00:56:03 +0300

    payment page done

--- src/index.js
+++ src/index.js
@@ -40,22 +40,72 @@ document.querySelectorAll('[data-product-quantity]').forEach(item => {
         totalAdjustedPrice();
     });
 });
-document.querySelectorAll('[data-remove-from-cart]').forEach(i =>{
-    i.addEventListener('click', ()=>{
+document.querySelectorAll('[data-remove-from-cart]').forEach(i => {
+    i.addEventListener('click', () => {
         i.closest('[data-product-info]').remove();
         totalAdjustedPrice();
     });
 });
 
-function totalAdjustedPrice(){
+function totalAdjustedPrice() {
     let totalOfAllProducts = 0;
-        document.querySelectorAll('[data-product-info]').forEach(product => {
-            const pricePerUnit = product.getAttribute('data-product-price');
-            const quantity = product.querySelector('[data-product-quantity]').value;
-            const totalPerUnit = quantity * pricePerUnit;
-            totalOfAllProducts += totalPerUnit;
-        });
-        document.getElementById('total-price-for-all-products').innerHTML = totalOfAllProducts;
+    document.querySelectorAll('[data-product-info]').forEach(product => {
+        const pricePerUnit = product.getAttribute('data-product-price');
+        const quantity = product.querySelector('[data-product-quantity]').value;
+        const totalPerUnit = quantity * pricePerUnit;
+        totalOfAllProducts += totalPerUnit;
+    });
+    document.getElementById('total-price-for-all-products').innerHTML = totalOfAllProducts;
 };
+
+const cityByCountry = {
+    eg: ['القاهرة', 'الاسكندرية'],
+    sa: ['الرياض', 'جدة'],
+    sy: ['دمشق', 'حلب', 'حماة'],
+    jo: ['عمان', 'العقبة'],
+};
+
+document.querySelectorAll('select[name="country"]').forEach(i => {
+    i.addEventListener('change', () => {
+        const country = i.value;
+        const cities = cityByCountry[country];
+
+        document.querySelectorAll('#choose-city option').forEach(i => i.remove());
+        const firstOption = document.createElement('option');
+        const firstOptionText = document.createTextNode('اختر مدينة');
+        firstOption.appendChild(firstOptionText);
+        firstOption.setAttribute('value', '');
+        firstOption.setAttribute('selected', 'selected');
+        firstOption.setAttribute('disabled', 'disabled');
+
+        const cityOptins = document.getElementById('choose-city');
+        cityOptins.appendChild(firstOption);
+
+        cities.forEach(i => {
+            const newOption = document.createElement('option');
+            const newOptionText = document.createTextNode(i);
+            newOption.appendChild(newOptionText);
+            newOption.setAttribute('value', i);
+            cityOptins.appendChild(newOption);
+        })
+    })
+})
+
+document.querySelectorAll('#form-checkout input[name="payment-method"]').forEach(i =>{
+    i.addEventListener('change', ()=>{
+        const paymentMethod = i.value;
+        const cardPaymentInput = document.querySelectorAll('#credit-card-info');
+        if (paymentMethod === 'on-delivery'){
+            cardPaymentInput.forEach(i => {
+                i.style.display='none'; 
+            });
+        }
+        else {
+            cardPaymentInput.forEach(i => {
+                i.style.display='flex'  
+            })
+        }
+    })
+})
 
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة سنة " + new Date().getFullYear() + "م";
